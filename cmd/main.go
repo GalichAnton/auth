80bf7eb35c60fdf991659c0fb48db3b31c472deb
aagalich <aagalich@avito.ru>
2024-02-10 11:15:00 +0300

    fast fix

--- cmd/main.go
+++ cmd/main.go
@@ -49,14 +49,14 @@ func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.
 	log.Printf("User with [id]=%d updated", req.GetId())
 	log.Printf("Context =%v", ctx)
 
-	return nil, nil
+	return &emptypb.Empty{}, nil
 }
 
 func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
 	log.Printf("User with [id]=%d deleted", req.GetId())
 	log.Printf("Context =%v", ctx)
 
-	return nil, nil
+	return &emptypb.Empty{}, nil
 }
 
 func main() {
